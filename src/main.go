a760848fb5c23161a50b0db2cdbe6e7612f79d85
Alejandro Andrade <meuxdev@gmail.com>
2022-05-10 18:54:39 -0500

    Variables, constants, zero values

--- src/main.go
+++ src/main.go
@@ -4,6 +4,33 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello World")
+	// declarative type
+	const pi float64 = 3.14
+	// Inferencial
+	const pi2 = 3.15
 
+	fmt.Println("Hello World")
+	fmt.Print("PI value: ")
+	fmt.Println(pi)
+	fmt.Println("PI second value", pi2)
+
+	// var integers
+	base := 12
+	var height int = 14
+	var area int
+
+	fmt.Println(base, height, area)
+
+	// zero values
+	var zeroValueInteger int
+	var zeroValueFloat float64
+	var zeroValueString string
+	var zeroValueBool bool
+
+	fmt.Println(zeroValueInteger, zeroValueFloat, zeroValueString, zeroValueBool)
+
+	// square area
+	const squareBase = 10
+	squareArea := squareBase * squareBase
+	fmt.Println("Square Area is: ", squareArea)
 }
