b283c67a14cb8b70e0e9ab1b9ebff1b4b53cd567
Alejandro Andrade <meuxdev@gmail.com>
2022-05-11 01:54:07 -0500

    Functions, return values, parameters, receive values

--- src/main.go
+++ src/main.go
@@ -5,6 +5,25 @@ import (
 	"math"
 )
 
+func sayHello(name string) {
+	fmt.Printf("Hello %s, How are you?\n", name)
+}
+
+func sayHelloWithAge(name string, age uint32, height, weight float64) {
+	fmt.Printf("Hello %s, \nYou have %d years\nYour height is %gcm\nYour weight is %gkg\n", name, age, height, weight)
+	// %g -> float32, float64, complex etc
+	// %d -> decimal
+	// %s  -> string
+}
+
+func double(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
 func pckgExampleFmt() {
 	helloMsg := "Hello"
 	worldMsg := "World"
@@ -20,6 +39,7 @@ func pckgExampleFmt() {
 	// %s -> for string
 	// %d -> for integer
 	// %v -> for any value
+	// %g -> for float32 complex etc
 	fmt.Printf("%v has %v years old\n", name, age)
 
 	// Springf
@@ -32,26 +52,29 @@ func pckgExampleFmt() {
 	fmt.Printf("Type of is age: %T \n", age)     // %T -> type of the var
 }
 
-func circleArea(radius float64) {
+func circleArea(radius float64) (result float64, errors bool) {
 	const pi float64 = 3.1416
 	area := pi * math.Pow(radius, 2)
 	fmt.Println("*************\nCircle Area\n*************\nRadius: ", radius)
 	fmt.Println("Area: ", area)
+	return area, false
 }
 
-func rectangleArea(sideX, sideY float64) {
+func rectangleArea(sideX, sideY float64) (result float64, err bool) {
 	area := sideY * sideX
 	fmt.Println("*************\nRectangle Area\n*************\nSide X: ", sideX)
 	fmt.Println("Side Y: ", sideY)
 	fmt.Println("Result: ", area)
+	return area, false
 }
 
-func trapezoidArea(top, bottom, height float64) {
+func trapezoidArea(top, bottom, height float64) (result float64, err bool) {
 	area := (top + bottom) * height / 2
 	fmt.Println("*************\n Trapezoid Area\n*************\n Top: ", top)
 	fmt.Println("Bottom: ", bottom)
 	fmt.Println("Height: ", height)
 	fmt.Println("Result: ", area)
+	return area, false
 }
 
 func main() {
@@ -117,8 +140,19 @@ func main() {
 	fmt.Println("Decrement: ", y)
 
 	// Challenge Area of rectangle, trapeze and circle
-	circleArea(3)
-	rectangleArea(12.3, 14.3)
+	resultAreaCircle, _ := circleArea(3)
+	resultAreaRectangle, errors := rectangleArea(12.3, 14.3)
 	trapezoidArea(12.3, 14.3, 12.3)
 	pckgExampleFmt()
+	sayHelloWithAge("Alejando", 24, 1.73, 74)
+
+	val1, val2 := doubleReturn(2)
+	fmt.Println("Value1 and value2 ", val1, val2)
+
+	// Escape for the value _ -> in case you dont need of the values that returns
+	_, double := doubleReturn(5)
+	fmt.Println("Double", double)
+	fmt.Println("Result Area Circle: ", resultAreaCircle)
+	fmt.Printf("Result Area Rectangle: %g\nErrors: %t ", resultAreaRectangle, errors)
+
 }
