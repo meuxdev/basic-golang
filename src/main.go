6245c9966b5270dadfd13239629baf279ddcfdb6
Alejandro Andrade <meuxdev@gmail.com>
2022-05-12 16:11:16 -0500

    if conditionals and challenge

--- src/main.go
+++ src/main.go
@@ -1,7 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func evenOrOdd() (odd, even bool) {
+	// chaleng odd or even
+	var numberString string = ""
+	var err any
+	var val int
+
+	for {
+		fmt.Println("Enter Your Number: ")
+		fmt.Scanln(&numberString)
+		val, err = strconv.Atoi(numberString)
+		if err == nil {
+			break
+		}
+	}
+
+	if val%2 == 0 {
+		fmt.Println("Is odd")
+		return true, false
+	} else {
+		fmt.Println("Is even")
+		return false, true
+	}
+}
+
+func passwordUser() {
+	// challenge to compare password and username
+	var inPass string
+	var inUser string
+	dbUser := "admin"
+	dbPass := "admin"
+
+	fmt.Println("Insert Username: ")
+	fmt.Scanln(&inUser)
+	fmt.Println("Insert Password")
+	fmt.Scanln(&inPass)
+
+	if inUser == dbUser && dbPass == inPass {
+		fmt.Println("Accessing to your account")
+	} else {
+		fmt.Println("Invalid credencials")
+	}
+
+}
 
 func main() {
-	fmt.Println("Clean Main")
+	val1 := 1
+	val2 := 2
+
+	if val1 == 1 {
+		fmt.Println("Value is 1")
+	} else {
+		fmt.Println("Value is NOT 1")
+	}
+
+	if val1 == 1 && val2 == 2 {
+		fmt.Println("Is true two conditions are true")
+	} else {
+		fmt.Println("One of the 2 conditions is NOT true")
+	}
+
+	if val1 == 0 || val2 == 2 {
+		fmt.Println("On of the two conditions is true")
+	}
+
+	// convert text to number
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Value: ", value)
+
+	// challenge odd or even
+
+	evenOrOdd()
+	passwordUser()
 }
