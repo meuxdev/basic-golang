aa788a1f6c3b8e375cbaf262e3d65269f6295279
Alejandro Andrade <meuxdev@gmail.com>
2022-05-14 02:04:39 -0500

    Structs: - Adding the struct of the car.

--- src/main.go
+++ src/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type Car struct {
+	brand string
+	model string
+	power uint
+	year  uint
+}
+
 func main() {
-	fmt.Println("Clean Main")
+
+	myCar := Car{brand: "Supra", model: "mk4"}
+	fmt.Println(myCar)
+
+	// other way to instance
+
+	var otherCar Car
+	otherCar.brand = "Ferrari"
+
+	fmt.Println(otherCar)
 }
