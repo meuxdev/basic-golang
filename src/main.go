0784a442a56415b4af2cdfe29fe030e92c9c84a5
Alejandro Andrade <meuxdev@gmail.com>
2022-05-14 01:37:53 -0500

    Slices and for range / palindrome challenge function

--- src/main.go
+++ src/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(text string) {
+	var textReverse string
+	// lower case to prevent error of mayus
+	text = strings.ToLower(text)
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i]) // we need to parse because the position returns the value in ASCII
+	}
+
+	if textReverse == text {
+		fmt.Println("Is palindrome")
+	} else {
+		fmt.Println("Is NOT palindrome")
+	}
+}
 
 func main() {
-	fmt.Println("Clean Main")
+	slice := []string{}
+	slice = append(slice, "Hello")
+	slice = append(slice, "how")
+	slice = append(slice, "are")
+	slice = append(slice, "you?")
+
+	for _, value := range slice {
+		fmt.Print(" ", value)
+	}
+
+	fmt.Println()
+
+	// for range returns
+	// index, value
+	for i := range slice {
+		fmt.Print(" ", i)
+	}
+
+	fmt.Println()
+
+	isPalindrome("ama")
+	isPalindrome("aMoR a roma")
+	isPalindrome("casas")
 }
