4c5db2c1c59e99d27537c8b4b24ea840c782211f
Alejandro Andrade <meuxdev@gmail.com>
2022-05-14 01:57:14 -0500

    Maps

--- src/main.go
+++ src/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Clean Main")
+	agesMap := make(map[string]int)
+
+	agesMap["Alejandro"] = 24
+	agesMap["Pepito"] = 19
+
+	fmt.Println("Ages Map: ", agesMap)
+
+	// range
+
+	// IMPORTANT: map does not has order to display
+	// maps use concurrency
+	// more efficient than slices
+
+	for i, value := range agesMap {
+		fmt.Println(i, value)
+	}
+
+	// find a value
+
+	ageValue, ok := agesMap["Pepito"]
+	fmt.Println(ageValue, ok)
+
+	ageValue, ok = agesMap["Josep"]
+	fmt.Println(ageValue, ok)
 }
