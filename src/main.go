a43e187f63e9a2e6e1aedf1a4fb7944e20690cfd
Alejandro Andrade <meuxdev@gmail.com>
2022-05-16 00:02:28 -0500

    Channels

--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func say(text string, c chan string) {
+func say(text string, c chan<- string) {
 	c <- text
 }
 
@@ -11,4 +11,6 @@ func main() {
 	c := make(chan string, 1)
 	fmt.Println("Hello")
 	go say("Bay", c)
+
+	fmt.Println(<-c)
 }
