0e46bc09cf51630d5d8439eeae4f9e7e61e4cc8a
Alejandro Andrade <meuxdev@gmail.com>
2022-05-15 23:58:48 -0500

    Adding the goroutine with channels

--- src/main.go
+++ src/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
+func say(text string, c chan string) {
+	c <- text
+}
+
 func main() {
-	// clean main
+	// type of val passing to the channel, amount of go routines the channel will receive
+	c := make(chan string, 1)
+	fmt.Println("Hello")
+	go say("Bay", c)
 }
