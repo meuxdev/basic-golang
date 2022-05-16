15ec26090de465c8be5514b1871f3d843c17e5f6
Alejandro Andrade <meuxdev@gmail.com>
2022-05-16 01:35:52 -0500

    Range, close, select with Channels -> How to handle a channel

--- src/main.go
+++ src/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func say(text string, c chan<- string) {
+	c <- text
+}
+
+func msg(text string, c chan<- string) {
 	c <- text
 }
 
@@ -12,5 +18,36 @@ func main() {
 	fmt.Println("Hello")
 	go say("Bay", c)
 
+	channel2 := make(chan string, 2)
+
+	channel2 <- "Msg 1"
+	channel2 <- "Msg 2"
+
+	// len() -> gives the len of the channel
+	// cap() -> capacity of the channel, how many you can add
+	fmt.Println(len(channel2), cap(channel2))
 	fmt.Println(<-c)
+
+	// Range and close
+	// channel2 <- "Msg 3"
+	close(channel2)
+
+	for msg := range channel2 {
+		fmt.Println(msg)
+	}
+
+	email := make(chan string)
+	email2 := make(chan string)
+	go msg("alex@gmail.com", email)
+	go msg("freddy@gmail.com", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email:
+			fmt.Println("Email received from", m1)
+		case m2 := <-email2:
+			fmt.Println("Email received from", m2)
+		}
+	}
+
 }
