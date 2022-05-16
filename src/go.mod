c630451b0380269e14ccbfd95c807c1c279ff11c
Alejandro Andrade <meuxdev@gmail.com>
2022-05-16 02:20:52 -0500

    Hello world with Echo Framework. Creating my first server with this module.
    On branch main
    Changes to be committed:
    	modified:   go.mod
    	modified:   go.sum
    	modified:   main.go

--- src/go.mod
+++ src/go.mod
@@ -3,10 +3,18 @@ module main
 go 1.18
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/yuin/goldmark v1.4.12 // indirect
+	golang.org/x/crypto v0.0.0-20220513210258-46612604a0f9 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
 	golang.org/x/net v0.0.0-20220513224357-95641704303c // indirect
 	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.10 // indirect
 	golang.org/x/tour v0.1.0 // indirect
 	golang.org/x/website/tour v0.0.0-20210616181959-e0d934b43647 // indirect
--- src/main.go
+++ src/main.go
@@ -1,53 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
-func say(text string, c chan<- string) {
-	c <- text
-}
-
-func msg(text string, c chan<- string) {
-	c <- text
-}
-
 func main() {
-	// type of val passing to the channel, amount of go routines the channel will receive
-	c := make(chan string, 1)
-	fmt.Println("Hello")
-	go say("Bay", c)
+	// Instance echo
 
-	channel2 := make(chan string, 2)
+	e := echo.New()
 
-	channel2 <- "Msg 1"
-	channel2 <- "Msg 2"
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	})
 
-	// len() -> gives the len of the channel
-	// cap() -> capacity of the channel, how many you can add
-	fmt.Println(len(channel2), cap(channel2))
-	fmt.Println(<-c)
-
-	// Range and close
-	// channel2 <- "Msg 3"
-	close(channel2)
-
-	for msg := range channel2 {
-		fmt.Println(msg)
-	}
-
-	email := make(chan string)
-	email2 := make(chan string)
-	go msg("alex@gmail.com", email)
-	go msg("freddy@gmail.com", email2)
-
-	for i := 0; i < 2; i++ {
-		select {
-		case m1 := <-email:
-			fmt.Println("Email received from", m1)
-		case m2 := <-email2:
-			fmt.Println("Email received from", m2)
-		}
-	}
-
+	e.Logger.Fatal(e.Start(":1323"))
 }
